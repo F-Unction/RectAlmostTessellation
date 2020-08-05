74f7d2bd8f6c1571a22b1af593f99060bc45f664
F-Unction <f_unction@qq.com>
2020-08-05 20:17:31 +0800

    sources 	new file:   RectAlmostTessellation/App.xaml 	new file:   RectAlmostTessellation/App.xaml.cs 	new file:   RectAlmostTessellation/AssemblyInfo.cs 	new file:   RectAlmostTessellation/MainWindow.xaml 	new file:   RectAlmostTessellation/MainWindow.xaml.cs 	new file:   RectAlmostTessellation/Rect.cs 	new file:   RectAlmostTessellation/RectAlmostTessellation.csproj 	new file:   RectAlmostTessellation/RectAlmostTessellation.csproj.user 	modified:   .gitignore

---
+++ RectAlmostTessellation/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace RectAlmostTessellation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Rect> rects = new List<Rect>();
+        private int RectNum = new int();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Start();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Start();
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            for (int i = 0; i < RectNum; i++)
+            {
+                rects[i].Done = false;
+            }
+            Tessellation(0, 0, (int)Rgn.ActualWidth, (int)Rgn.ActualWidth);
+            ShowRects();
+        }
+
+        private void Start()
+        {
+            if (int.TryParse(NumTextBox.Text, out RectNum))
+            {
+                GenRandonRects();
+                rects.Sort();
+                Tessellation(0, 0, (int)Rgn.ActualWidth, (int)Rgn.ActualWidth);
+            }
+            ShowRects();
+        }
+
+        private void GenRandonRects()
+        {
+            rects.Clear();
+            for (var i = 0; i < RectNum; i++)
+            {
+                Random rd = new Random();
+                rects.Add(new Rect(0, 0, rd.Next(1, 7) * 20, rd.Next(1, 5) * 20));
+            }
+        }
+
+        private void Tessellation(int RgnLeft, int RgnTop, int RgnW, int RgnH)
+        {
+            if (RectNum != 0)
+            {
+                int i;
+                for (i = 0; i < RectNum; i++)
+                {
+                    if (!rects[i].Done && rects[i].Width <= RgnW && rects[i].Height <= RgnH)
+                    {
+                        if (i >= RectNum || i < 0)
+                        {
+                            return;
+                        }
+                        if (i == RectNum && rects[i].Done)
+                        {
+                            return;
+                        }
+                        rects[i].X = RgnLeft;
+                        rects[i].Y = RgnTop;
+                        rects[i].Done = true;
+
+                        Tessellation(RgnLeft + rects[i].Width, RgnTop, RgnW - rects[i].Width, rects[i].Height);
+                        Tessellation(RgnLeft, RgnTop + rects[i].Height, RgnW, RgnH - rects[i].Height);
+
+                        break;
+                    }
+                }
+             
+            }
+        }
+
+        private void ShowRects()
+        {
+            Rgn.Children.Clear();
+            foreach (var i in rects)
+            {
+                PaintRect(i);
+            }
+        }
+
+        private void PaintRect(Rect rect)
+        {
+            if (rect.Done)
+            {
+                var trect = new Canvas
+                {
+                    Background = new SolidColorBrush(rect.Color),
+                    Width = rect.Width,
+                    Height = rect.Height
+                };
+                Canvas.SetLeft(trect, rect.X);
+                Canvas.SetTop(trect, rect.Y);
+                Rgn.Children.Add(trect);
+            }
+        }
+    }
+}
---
+++ RectAlmostTessellation/Rect.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows.Media;
+
+public class Rect : IComparable<Rect>
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public Color Color { get; set; }
+    public bool Done { get; set; }
+
+    public Rect(int x, int y, int width, int height)
+    {
+        Random rd = new Random();
+
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+        Done = false;
+        Color = Color.FromArgb(255, (byte)rd.Next(0, 255), (byte)rd.Next(0, 255), (byte)rd.Next(0, 255));
+    }
+
+    public int CompareTo([AllowNull] Rect other)
+    {
+        if (this.Height > other.Height)
+        {
+            return -1;
+        }
+        else if (this.Height < other.Height)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
